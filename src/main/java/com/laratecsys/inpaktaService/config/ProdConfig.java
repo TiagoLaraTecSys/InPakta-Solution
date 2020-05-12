a83307a6dbfa89b04cbc1fcc5b9049e9a90d4b3c
Tiago Ribeiro <laratecsys@gmail.com>
2020-05-11 23:05:49 -0300

    Difinindo ambientes de desenvolvimento

---
+++ src/main/java/com/laratecsys/inpaktaService/config/ProdConfig.java
@@ -0,0 +1,36 @@
+package com.laratecsys.inpaktaService.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.laratecsys.inpaktaService.Service.EmailService;
+import com.laratecsys.inpaktaService.Service.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		} else {
+
+			return true;
+		}
+
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
