bd6b39aefbf01b0fc094997eec6a7f075a43d13d
Tiago Ribeiro <laratecsys@gmail.com>
2020-05-11 23:16:30 -0300

    alterando resources

--- src/main/java/com/laratecsys/inpaktaService/config/ProdConfig.java
+++ src/main/java/com/laratecsys/inpaktaService/config/ProdConfig.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.laratecsys.inpaktaService.Service.EmailService;
 import com.laratecsys.inpaktaService.Service.SmtpEmailService;
@@ -32,5 +35,14 @@ public class ProdConfig {
 	@Bean
 	public EmailService emailService() {
 		return new SmtpEmailService();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
 	}
 }
