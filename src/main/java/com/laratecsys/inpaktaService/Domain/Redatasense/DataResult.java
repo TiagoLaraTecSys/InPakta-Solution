74e0cd4c92ef9b62b699f79efe75d8b10ce62a0d
Tiago Ribeiro <laratecsys@gmail.com>
2020-06-02 22:37:10 -0300

    Criação das entidades que irão armazenar os resultados e a qual base de dados prtence esses resultados.

---
+++ src/main/java/com/laratecsys/inpaktaService/Domain/Redatasense/DataResult.java
@@ -0,0 +1,194 @@
+package com.laratecsys.inpaktaService.Domain.Redatasense;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class DataResult {
+
+	@Id
+	Integer id_data;
+	Integer run_id;
+	Date	run_timestamp;
+	String 	schema_name;
+	String 	table_name;
+	String 	column_name;
+	double 	probability;
+	String 	model;
+	String 	model_mode;
+	String 	dictionary;
+	Integer	num_rows;
+	Integer	score;
+	String 	sample_data;
+	
+	@ManyToOne()
+	@JoinColumn(name = "id_db")
+	private DbProperties dbProperties;
+	
+	public DataResult(){
+		
+	}
+
+	public DataResult(Integer id_data, Integer run_id, Date run_timestamp, String schema_name, String table_name,
+			String column_name, double probability, String model, String model_mode, String dictionary,
+			Integer num_rows, Integer score, String sample_data) {
+		super();
+		this.id_data = id_data;
+		this.run_id = run_id;
+		this.run_timestamp = run_timestamp;
+		this.schema_name = schema_name;
+		this.table_name = table_name;
+		this.column_name = column_name;
+		this.probability = probability;
+		this.model = model;
+		this.model_mode = model_mode;
+		this.dictionary = dictionary;
+		this.num_rows = num_rows;
+		this.score = score;
+		this.sample_data = sample_data;
+	}
+
+	public Integer getId_data() {
+		return id_data;
+	}
+
+	public void setId_data(Integer id_data) {
+		this.id_data = id_data;
+	}
+
+	public Integer getRun_id() {
+		return run_id;
+	}
+
+	public void setRun_id(Integer run_id) {
+		this.run_id = run_id;
+	}
+
+	public Date getRun_timestamp() {
+		return run_timestamp;
+	}
+
+	public void setRun_timestamp(Date run_timestamp) {
+		this.run_timestamp = run_timestamp;
+	}
+
+	public String getSchema_name() {
+		return schema_name;
+	}
+
+	public void setSchema_name(String schema_name) {
+		this.schema_name = schema_name;
+	}
+
+	public String getTable_name() {
+		return table_name;
+	}
+
+	public void setTable_name(String table_name) {
+		this.table_name = table_name;
+	}
+
+	public String getColumn_name() {
+		return column_name;
+	}
+
+	public void setColumn_name(String column_name) {
+		this.column_name = column_name;
+	}
+
+	public double getProbability() {
+		return probability;
+	}
+
+	public void setProbability(double probability) {
+		this.probability = probability;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getModel_mode() {
+		return model_mode;
+	}
+
+	public void setModel_mode(String model_mode) {
+		this.model_mode = model_mode;
+	}
+
+	public String getDictionary() {
+		return dictionary;
+	}
+
+	public void setDictionary(String dictionary) {
+		this.dictionary = dictionary;
+	}
+
+	public Integer getNum_rows() {
+		return num_rows;
+	}
+
+	public void setNum_rows(Integer num_rows) {
+		this.num_rows = num_rows;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public String getSample_data() {
+		return sample_data;
+	}
+
+	public void setSample_data(String sample_data) {
+		this.sample_data = sample_data;
+	}
+	
+	public DbProperties getDbProperties() {
+		return dbProperties;
+	}
+
+	public void setDbProperties(DbProperties dbProperties) {
+		this.dbProperties = dbProperties;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_data == null) ? 0 : id_data.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DataResult other = (DataResult) obj;
+		if (id_data == null) {
+			if (other.id_data != null)
+				return false;
+		} else if (!id_data.equals(other.id_data))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/laratecsys/inpaktaService/Domain/Redatasense/DbProperties.java
@@ -0,0 +1,152 @@
+package com.laratecsys.inpaktaService.Domain.Redatasense;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class DbProperties {
+	
+	@Id
+	Integer id_db;
+	String	repository_name;
+	String	vendor;
+	String	driver;
+	String	username;
+	String	password;
+	String	dbschema;
+	String	url;
+	Integer	isActive;
+	
+	@OneToMany
+	private List<DataResult> resultados = new ArrayList<>();
+	
+	
+	public DbProperties() {
+		
+	}
+
+	public DbProperties(Integer id_db, String repository_name, String vendor, String driver, String username,
+			String password, String dbschema, String url, Integer isActive) {
+		super();
+		this.id_db = id_db;
+		this.repository_name = repository_name;
+		this.vendor = vendor;
+		this.driver = driver;
+		this.username = username;
+		this.password = password;
+		this.dbschema = dbschema;
+		this.url = url;
+		this.isActive = isActive;
+	}
+
+	public Integer getId_db() {
+		return id_db;
+	}
+
+	public void setId_db(Integer id_db) {
+		this.id_db = id_db;
+	}
+
+	public String getRepository_name() {
+		return repository_name;
+	}
+
+	public void setRepository_name(String repository_name) {
+		this.repository_name = repository_name;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+	public String getDriver() {
+		return driver;
+	}
+
+	public void setDriver(String driver) {
+		this.driver = driver;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDbschema() {
+		return dbschema;
+	}
+
+	public void setDbschema(String dbschema) {
+		this.dbschema = dbschema;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Integer getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Integer isActive) {
+		this.isActive = isActive;
+	}
+	
+	public List<DataResult> getResultados() {
+		return resultados;
+	}
+
+	public void setResultados(List<DataResult> resultados) {
+		this.resultados = resultados;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_db == null) ? 0 : id_db.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DbProperties other = (DbProperties) obj;
+		if (id_db == null) {
+			if (other.id_db != null)
+				return false;
+		} else if (!id_db.equals(other.id_db))
+			return false;
+		return true;
+	}
+	
+	
+}
