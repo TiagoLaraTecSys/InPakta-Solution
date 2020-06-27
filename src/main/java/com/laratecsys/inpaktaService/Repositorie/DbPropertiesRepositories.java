c1bbcae5eb4d87dfef382d968f4d389f1c5f5d48
tiago de lara ribeiro <laratecsys@gmail.com>
2020-06-27 17:12:05 -0300

    Documentação API com Swagger Heroku

--- src/main/java/com/laratecsys/inpaktaService/Repositorie/DbPropertiesRepositories.java
+++ src/main/java/com/laratecsys/inpaktaService/Repositorie/DbPropertiesRepositories.java
@@ -1,11 +1,13 @@
 package com.laratecsys.inpaktaService.Repositorie;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.laratecsys.inpaktaService.Domain.Redatasense.DbProperties;
 
 public interface DbPropertiesRepositories extends JpaRepository<DbProperties, Integer> {
 	
-	
+	public List<DbProperties> findByClienteId(Integer id);
 
 }
