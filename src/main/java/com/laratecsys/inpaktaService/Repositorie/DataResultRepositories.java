5e1e70295cad4f03d0c073c8661cb1689c09412d
tiago de lara ribeiro <laratecsys@gmail.com>
2020-08-29 17:35:56 -0300

    Trying to make a multiple Join

--- src/main/java/com/laratecsys/inpaktaService/Repositorie/DataResultRepositories.java
+++ src/main/java/com/laratecsys/inpaktaService/Repositorie/DataResultRepositories.java
@@ -3,12 +3,17 @@ package com.laratecsys.inpaktaService.Repositorie;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.laratecsys.inpaktaService.Domain.Cliente;
 import com.laratecsys.inpaktaService.Domain.Redatasense.DataResult;
 
 public interface DataResultRepositories extends JpaRepository<DataResult, Integer> {
 	
+	@Query("SELECT dr FROM DataResult dr"
+			+ " JOIN dr.dbProperties dbp"
+			+ " JOIN dbp.cliente c "
+			+ " where c.id IN :id and dbp.isActive=1")
 	public List<DataResult>findByClienteId(Cliente id);
-
+	
 }
