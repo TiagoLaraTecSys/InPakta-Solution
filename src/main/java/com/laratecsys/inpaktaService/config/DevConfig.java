79a2c18854692604a7bc5d46dd642df76a3135f2
tiago de lara ribeiro <laratecsys@gmail.com>
2020-08-30 18:47:15 -0300

    new Feature File Mapping

--- src/main/java/com/laratecsys/inpaktaService/config/DevConfig.java
+++ src/main/java/com/laratecsys/inpaktaService/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.laratecsys.inpaktaService.config;
 
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,8 +10,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.laratecsys.inpaktaService.Domain.Cliente;
 import com.laratecsys.inpaktaService.Domain.Redatasense.DatadiscoveryProperties;
+import com.laratecsys.inpaktaService.Domain.Redatasense.FileProperties;
 import com.laratecsys.inpaktaService.Repositorie.DatadiscoveryPropertiesRepositories;
+import com.laratecsys.inpaktaService.Repositorie.FilePropertiesRepositories;
 import com.laratecsys.inpaktaService.Service.EmailService;
 import com.laratecsys.inpaktaService.Service.SmtpEmailService;
 
@@ -22,15 +27,24 @@ public class DevConfig {
 
 	@Autowired
 	private DatadiscoveryPropertiesRepositories dataRepo;
+	
+	@Autowired
+	private FilePropertiesRepositories filePropertiesRepositories;
 	@Bean
 	public boolean instantiateDevDatabase() throws ParseException {
 
 		if ("update".equals(strategy)) {
-			DatadiscoveryProperties newDatadiscoveryProperties = new DatadiscoveryProperties(null, 0.6,
+			/*DatadiscoveryProperties newDatadiscoveryProperties = new DatadiscoveryProperties(null, 0.6,
 					"pt-ner-multi.bin", "en-token.bin", "MYTABLE_TO_IGNORE", 1000, 6, 3, 0,
 					"com.strider.datadefender.specialcase.SinDetector.detectSin", "model_generic", "nomes.xml",
-					"NERRegex");
+					"NERRegex");*/
 			//dataRepo.save(newDatadiscoveryProperties);
+			
+			FileProperties newFileProperties = new FileProperties(null,"0.6", "pt-ner-multi.bin", "en-token.bin", "model_generic", "C:/Users/larat/OneDrive/Documents/InPakta/teste", 
+					"excluded_files.txt","nomdes.xml", "txt, TXT, docx,doc,PDF,pdf,html,css", "NEREntropy",new Date(System.currentTimeMillis()),new Cliente(105, null, null, null, null));
+			
+			filePropertiesRepositories.saveAll(Arrays.asList(newFileProperties));
+			
 			return false;
 		} else {
 
