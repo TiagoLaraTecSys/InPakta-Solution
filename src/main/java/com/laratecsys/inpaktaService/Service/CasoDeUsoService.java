903939b777de9da1320cbe42d22c03f9339e2a74
tiago de lara ribeiro <laratecsys@gmail.com>
2020-09-20 12:28:12 -0300

    Up

--- src/main/java/com/laratecsys/inpaktaService/Service/CasoDeUsoService.java
+++ src/main/java/com/laratecsys/inpaktaService/Service/CasoDeUsoService.java
@@ -3,6 +3,7 @@ package com.laratecsys.inpaktaService.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.laratecsys.inpaktaService.Domain.Cliente;
 import com.laratecsys.inpaktaService.Domain.Redatasense.ERP.CasoDeUso;
@@ -10,6 +11,7 @@ import com.laratecsys.inpaktaService.Repositorie.CasoDeUsoRepositories;
 import com.laratecsys.inpaktaService.Security.UserSS;
 import com.laratecsys.inpaktaService.Service.exception.AuthorizationException;
 
+@Service
 public class CasoDeUsoService {
 	
 	@Autowired
